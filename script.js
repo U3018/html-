6590f38a6fd864a289cf2e401e74ccbde5fb6f90
ushik <167966113+U3018@users.noreply.github.com>
2026-05-15 23:42:58 +0900

    5/15 23:30

---
+++ script.js
@@ -0,0 +1,6 @@
+function new_page() { 
+    window.open("https://www.google.com/?hl=ja","","");
+}
+function new_page2() { 
+    window.open("https://www.google.com/?hl=ja","google_window","width=400,height=400,scrollbars=yes");
+}
